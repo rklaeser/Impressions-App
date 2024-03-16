dab3fdb1552e9a34196e9c2fa5af6983a1cbc69c
Reed Klaeser <reed.klaeser@gmail.com>
2024-03-16 13:14:19 -0700

    Camera View container exists

---
+++ Impress UIKit/Hw8/VC_CameraContainer.swift
@@ -0,0 +1,39 @@
+//
+//  SwiftUIContainerViewController.swift
+//  Hw8
+//
+//  Created by Reed Klaeser on 3/12/24.
+//
+import UIKit
+import SwiftUI
+
+class VC_CameraContainer: UIViewController {
+    
+    @IBOutlet var containerViewCamera: UIView!
+    
+    
+    var mySwiftUIViewInstance: V_Camera?
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            mySwiftUIViewInstance = V_Camera() // Initialize your SwiftUI view
+
+            // Set up container view
+            if let mySwiftUIViewInstance = mySwiftUIViewInstance {
+                let hostingController = UIHostingController(rootView: mySwiftUIViewInstance)
+                addChild(hostingController)
+                containerViewCamera.addSubview(hostingController.view)
+                hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+                NSLayoutConstraint.activate([
+                    hostingController.view.leadingAnchor.constraint(equalTo: containerViewCamera.leadingAnchor),
+                    hostingController.view.trailingAnchor.constraint(equalTo: containerViewCamera.trailingAnchor),
+                    hostingController.view.topAnchor.constraint(equalTo: containerViewCamera.topAnchor),
+                    hostingController.view.bottomAnchor.constraint(equalTo: containerViewCamera.bottomAnchor)
+                ])
+                hostingController.didMove(toParent: self)
+            }
+        }
+
+    
+}
+
---
+++ Impress UIKit/Hw8/V_Camera.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct V_Camera: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct V_Camera_Preview: PreviewProvider {
+    static var previews: some View {
+        V_Camera()
+    }
+}
